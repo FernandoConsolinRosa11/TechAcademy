b52f9ed95997aab2a384c835880054958d80d43a
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2025-03-11 22:47:38 -0300

    Add files via upload

---
+++ aula02/index.js
@@ -0,0 +1,30 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Informe seu nome:");
+console.log("É um prazer conhece-lo: "+ nome + "\n");
+
+let cor = prompt("Qual é a sua cor favorita? ");
+console.log(cor + " é uma ótima cor!"+ "\n");
+
+let filme = prompt("Qual é seu filme favorito? ");
+console.log(filme + " é uma obra-prima!" + "\n");
+
+let cidade = prompt("Qual cidade voce mora? ");
+console.log(cidade + "\n");
+
+let pet = prompt("Qual é seu pet? ");
+console.log(pet + "\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
